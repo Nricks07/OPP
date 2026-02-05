245eedfc694adeacd79aab95759a8c1a8f792362
Nricks07 <enriquelr2804@gmail.com>
2026-02-05 09:33:45 -0600

    Add files via upload

---
+++ piton/main_invent.py
@@ -0,0 +1,13 @@
+from SistemaInven import *
+
+prod1 = Producto("Maruchan", 16, 100)
+prod2 = Producto("Coca Cola", 18, 50)
+prod3 = Producto("Pan Bimbo", 35, 30)
+prod4 = Producto("Leche Lala", 25, 40)
+prod5 = Producto("Huevos", 42, 20)
+prod6 = Producto("Arroz", 28, 60)
+prod7 = Producto("Frijoles", 30, 45)
+prod8 = Producto("Aceite", 48, 25)
+prod9 = Producto("Azúcar", 27, 55)
+prod10 = Producto("Café", 60, 15)
+
